e96961ede28d10823ee40120c7aeef7ecec6fa72
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2021-03-13 14:30:23 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,12 @@
+<?
+
+
+$uag = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36 Edg/89.0.774.50
+";
+
+
+$cook = "PHPSESSID=f71f5efcf2b134a6399651918cb19e13; AccExist=2840";
+
+
+
+?>
